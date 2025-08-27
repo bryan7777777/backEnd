253ad00b24e92e462862f00eab8ea6d0e20b3859
bryan7777777 <bryannagano15@gmail.com>
2025-08-27 16:41:27 -0300

    pasta 6 criada atividade da idade feito em case

---
+++ pastaDeTestes/back6Idade/pages/desconto.php
@@ -0,0 +1,50 @@
+<?php
+$nome = filter_input(INPUT_GET, "nome");
+$idade = filter_input(INPUT_GET, "idade", FILTER_VALIDATE_INT);
+
+switch ($idade) {
+    case $idade >= 60:
+        $declaracao = "Melhor idade ";
+        $descricao = "você é bem vindo ";
+        break;
+    
+    case $idade >= 26:
+        $declaracao = "Adulto ";
+        $descricao = "você é bem vindo ";
+        break;
+    
+    case $idade >= 18:
+        $declaracao = "Joven ";
+        $descricao = "você é bem vindo ";
+        break;
+    
+    case $idade >= 12:
+        $declaracao = "Adolescente ";
+        $descricao = "você não pode entrar ";
+        break;
+    
+    case $idade >= 11:
+        $declaracao = "Crinça ";
+        $descricao = "você não pode entrar ";
+        break;
+};
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/desconto.css">
+    <title>Desconto</title>
+</head>
+
+<body>
+    <div id="resultado">
+        <h1>Sua idade</h1>
+        <?= "Senhor(a) " . $nome . " sua idade é " . $idade . ", você é considerado " . $declaracao . "por tanto, " . $descricao ?>
+    </div>
+</body>
+
+</html>
