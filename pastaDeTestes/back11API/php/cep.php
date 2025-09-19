913e9d1ba46d89242bee720c01a10b9324032c18
bryan7777777 <bryannagano15@gmail.com>
2025-09-19 17:25:24 -0300

    up

--- pastaDeTestes/back11API/php/cep.php
+++ pastaDeTestes/back11API/php/cep.php
@@ -1,3 +1,25 @@
 <?php 
 
+$cep = trim(filter_input(INPUT_GET,"cepBusca", FILTER_VALIDATE_INT));
+$result = "";
+
+switch ($cep) {
+    case !isset($cep) || strlen($cep) != 8:
+        $result = "ERRO, VALORES INVALIDOS";
+        break;
+    
+    default:
+    $url = "https://viacep.com.br/ws/{$cep}/json/";
+
+    $options = [
+        "http" => [
+            "method" => "GET",
+            "header" => "Content-Type: application/json"
+        ]
+    ];
+    $context = stream_context_create($options);
+    $response = file_get_contents($url, false, $context);
+        break;
+}
+
 ?>
