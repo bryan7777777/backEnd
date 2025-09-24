5dad08dd22d54957ec541505d6780d0df4e38f50
bryan7777777 <bryannagano15@gmail.com>
2025-09-24 14:16:43 -0300

    api

--- pastaDeTestes/back11API/php/cep.php
+++ pastaDeTestes/back11API/php/cep.php
@@ -1,25 +1,58 @@
-<?php 
+<?php
+$cep = preg_replace('/\D/', '', $_GET['cepBuscado'] ?? '');
+$mensagem = "";
+$url = "";
 
-$cep = trim(filter_input(INPUT_GET,"cepBusca", FILTER_VALIDATE_INT));
-$result = "";
-
-switch ($cep) {
-    case !isset($cep) || strlen($cep) != 8:
-        $result = "ERRO, VALORES INVALIDOS";
-        break;
-    
-    default:
+if (!isset($cep) || strlen($cep) != 8) {
+    $mensagem = "ERRO, VALORES INVÁLIDOS";
+} else {
     $url = "https://viacep.com.br/ws/{$cep}/json/";
-
     $options = [
         "http" => [
             "method" => "GET",
             "header" => "Content-Type: application/json"
         ]
     ];
+
     $context = stream_context_create($options);
     $response = file_get_contents($url, false, $context);
-        break;
-}
 
+    if ($response === false) {
+        $mensagem = "Erro ao acessar a API ViaCEP.";
+    } else {
+        $dados = json_decode($response, true);
+
+        if (isset($dados["erro"])) {
+            $mensagem = "CEP não encontrado.";
+        }
+    }
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Consulta de CEP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <span id="erro"><?= $mensagem ?></span>
+    <div>
+        <input type="text" value="<?= isset($dados['logradouro']) ? $dados['logradouro'] : "" ?>">
+        <input type="text" value="<?= isset($dados['complemento']) ? $dados['complemento'] : "" ?>">
+        <input type="text" value="<?= isset($dados['bairro']) ? $dados['bairro'] : "" ?>">
+        <input type="text" value="<?= isset($dados['localidade']) ? $dados['localidade'] : "" ?>">
+        <input type="text" value="<?= isset($dados['estado']) ? $dados['estado'] : "" ?>">
+
+        <!-- <div><?= isset($dados['logradouro']) ? $dados['logradouro'] : "" ?></div>
+        <div><?= isset($dados['logradouro']) ? $dados['complemento'] : "" ?></div>
+        <div><?= isset($dados['logradouro']) ? $dados['bairro'] : "" ?></div>
+        <div><?= isset($dados['logradouro']) ? $dados['localidade'] : "" ?></div>
+        <div><?= isset($dados['logradouro']) ? $dados['estado'] : "" ?></div> -->
+    </div>
+</body>
+
+</html>
