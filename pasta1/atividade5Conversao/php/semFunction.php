3ff58fb98f75e81ce3dc721269b5d7f976ddabc3
bryan7777777 <bryannagano15@gmail.com>
2025-10-02 17:06:25 -0300

    nota add semFunction

---
+++ pasta1/atividade5Conversao/php/semFunction.php
@@ -0,0 +1,21 @@
+<!-- Criar um algoritmo em php que permita três tipos de conversão:
+- Conversão de Real para Dólar
+- Conversão de Real para Euro
+- Conversão de Real para qualquer moeda que vocês escolherem
+ 
+O usuário deverá informar o valor em real e escolher em qual moeda deseja ver a
+conversão. Importante realizar todas as validações e impedir que erros ocorram.
+ 
+Usar o conceito de function para validar as entradas, exibir as mensagens, e para
+realizar a conversão.
+ 
+Importante
+- Entrega feita no GitHub
+- O repositório deverá ter os seguintes commits
+     - estruturação do html e estilização com css
+     - criação da página php sem functions mas funcional
+     - criação da função que valida entradas
+     - criação da função que exibe a mensagem
+     - criação da função que converte para dólar
+     - criação da função que converte para euro
+     - criação da função que converte para a moeda de sua escolha  -->
