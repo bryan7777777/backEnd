089cf156908dd97926926381c7f50783fb4a79d0
bryan7777777 <bryannagano15@gmail.com>
2025-11-18 17:26:16 -0300

    up

--- bancoDeDados/clinicaVet/atv10.sql
+++ bancoDeDados/clinicaVet/atv10.sql
@@ -28,3 +28,46 @@ CREATE TABLE produtos (
 );
 
 SELECT * FROM produtos
+
+
+
+
+CREATE DATABASE sistema_imobiliaria;
+
+USE sistema_imobiliaria;
+
+CREATE TABLE cliente (
+idCliente INT PRIMARY KEY AUTO_INCREMENT,
+nomeCliente VARCHAR(50) NOT NULL,
+cpf CHAR(11) NOT NULL UNIQUE,
+celular CHAR(11) NOT NULL,
+email VARCHAR(50) NOT NULL,
+cidade VARCHAR(50) NOT NULL,
+estado CHAR(2) NOT NULL,
+cep CHAR(8),
+tipoLogradouro VARCHAR(15) NOT NULL,
+nomeLogradouro VARCHAR(50) NOT NULL,
+numero VARCHAR(6) NOT NULL,
+complemento VARCHAR(30)
+);
+
+CREATE TABLE imovel(
+idImovel INT PRIMARY KEY AUTO_INCREMENT,
+idCliente INT NOT NULL,
+tipoImovel VARCHAR(50) NOT NULL,
+finalidade VARCHAR(50) NOT NULL,
+localizacao VARCHAR(50) NOT NULL,
+areaCostruida DECIMAL(10,2) NOT NULL,
+areaTerreno DECIMAL(10,2) NOT NULL,
+qtdQuartos INT NOT NULL,
+qtdBanheiros INT NOT NULL,
+qtdGaragem INT NOT NULL,
+descricao VARCHAR(50) NOT NULL,
+CONSTRAINT fk_imovel_cliente FOREIGN KEY (idCliente)
+REFERENCES cliente(idCliente)
+);
+
+DROP TABLE imovel;
+
+SELECT * FROM cliente;
+SELECT * FROM imovel
