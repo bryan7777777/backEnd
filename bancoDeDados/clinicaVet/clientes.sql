0e62de9751a09a67d8c13a45048e82d347e5ff43
bryan7777777 <bryannagano15@gmail.com>
2026-02-10 13:59:22 -0300

    up

--- bancoDeDados/clinicaVet/clientes.sql
+++ bancoDeDados/clinicaVet/clientes.sql
@@ -11,3 +11,54 @@ CREATE TABLE clientes (
 );
 
 SELECT * FROM clientes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+DELIMITER //
+
+CREATE PROCEDURE attEstoqueAluguel(
+    IN idCli INT,
+    IN idFunc INT,
+    IN idItem INT,
+    IN qtdItens INT,
+    IN valorIT DECIMAL(10,2),
+    IN valorUNI DECIMAL(10,2)
+)
+BEGIN
+    START TRANSACTION;
+    
+    -- Atualiza o estoque
+    UPDATE equipamento
+    SET qtd = qtd - qtdItens
+    WHERE idEquipamento = idItem;
+    
+    -- Insere o aluguel
+    INSERT INTO aluguel (idCliente, idFuncionario, dataHoraRetirada)
+    VALUES (idCli, idFunc, NOW());
+    
+    -- Insere os detalhes do equipamento alugado
+    INSERT INTO aluguelEquipamento (idEquipamento, idAluguel, valorItem, valorUnitario, qtd)
+    VALUES (idItem, LAST_INSERT_ID(), valorIT, valorUNI, qtdItens);
+    
+    COMMIT;
+END //
+
+DELIMITER ;
+
+CALL attEstoqueAluguel(1, 2, 3, 5, 100.00, 20.00);
